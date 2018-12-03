6cfd9a69138b8a351238a499e6aaf0cb1a5601d2
oneightwo <oneightwo@mail.ru>
2018-12-03 23:54:22 +0400

    + icon cardView image

--- app/src/main/java/ru/oshkin/vk_feed/newsLine/AdapterNewsImage.kt
+++ app/src/main/java/ru/oshkin/vk_feed/newsLine/AdapterNewsImage.kt
@@ -1,6 +1,5 @@
 package ru.oshkin.vk_feed.newsLine
 
-import android.Manifest
 import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -8,16 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import com.squareup.picasso.Picasso
-import pub.devrel.easypermissions.EasyPermissions
-import pub.devrel.easypermissions.PermissionRequest
 import ru.oshkin.vk_feed.R
 import ru.oshkin.vk_feed.retrofit.Photo
 import ru.oshkin.vk_feed.retrofit.PhotoSize
+import ru.oshkin.vk_feed.tools.setVisible
 
-class AdapterNewsImage(photo: List<Photo>, private val activity: NewsActivity): RecyclerView.Adapter<AdapterNewsImage.ImageViewHolder>() {
+class AdapterNewsImage(photo: List<Photo>, private val activity: NewsActivity) :
+    RecyclerView.Adapter<AdapterNewsImage.ImageViewHolder>() {
 
     private val photo = ArrayList(photo)
-    private val cardHeight = with(activity.resources) { getDimensionPixelSize(R.dimen.recycler_view_height) - getDimensionPixelSize(R.dimen.card_margin) }
+    private val cardHeight =
+        with(activity.resources) { getDimensionPixelSize(R.dimen.recycler_view_height) - getDimensionPixelSize(R.dimen.card_margin) }
 
     override fun onCreateViewHolder(viewHolder: ViewGroup, p1: Int): ImageViewHolder {
         val itemView = LayoutInflater.from(viewHolder.context).inflate(R.layout.recycler_item_image, viewHolder, false)
@@ -31,7 +31,12 @@ class AdapterNewsImage(photo: List<Photo>, private val activity: NewsActivity): 
         val width = getWidth(optimalPhoto)
         with(imageViewHolder) {
             cardView.layoutParams.width = width
-            Picasso.get().load(optimalPhoto.url).resize(width, cardHeight).into(imageViewHolder.imageViewNews)
+            if (position == 0) {
+                imageViewLibrary.setVisible(true)
+            } else {
+                imageViewLibrary.setVisible(false)
+            }
+            Picasso.get().load(optimalPhoto.url).resize(width, cardHeight).into(imageViewNews)
         }
     }
 
@@ -40,11 +45,12 @@ class AdapterNewsImage(photo: List<Photo>, private val activity: NewsActivity): 
 
 
     inner class ImageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
-        var imageViewNews: ImageView = view.findViewById(R.id.imageNews_cv)
+        val imageViewLibrary: ImageView = view.findViewById(R.id.library_image_iv)
+        val imageViewNews: ImageView = view.findViewById(R.id.imageNews_cv)
         val cardView: CardView = view.findViewById(R.id.imageCardView)
+
         init {
-            imageViewNews.setOnClickListener{
+            imageViewNews.setOnClickListener {
                 val url = photo[adapterPosition].getMaxPhoto().url
                 ImageActivity.outputImage(activity, url)
             }
